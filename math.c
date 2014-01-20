977f51a26544d386646fca24ec822c41f9a66dc3
Ronil Bhimjiani <P12225353@GH583-012.lec-admin.dmu.ac.uk>
2014-01-20 12:30:29 +0000

    first commit

---
+++ math.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    printf("Enter value for i ? ");
+    scanf("%i", &i);
+    printf("Enter value for j ? ");
+    scanf("%i", &j);
+    
+    printf("i+j = %i\n", i+j); 
+    
+    printf("i-j = %i\n", i-j); 
+    
+    printf("i*j = %i\n", i*j); 
+    
+    return 0;
+}
+
+
